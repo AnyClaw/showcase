110ea3dbd859b141679d9d339b684a72be24a478
AnyClaw <l.kolryser@yandex.ru>
2026-03-23 15:18:00 +0300

    added swagger documentation

--- src/main/java/com/example/showcase/controller/UserController.java
+++ src/main/java/com/example/showcase/controller/UserController.java
@@ -1,24 +1,42 @@
 package com.example.showcase.controller;
 
 import com.example.showcase.dto.UserDTO;
-import com.example.showcase.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/user")
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserService userService;
+public interface UserController {
 
     @GetMapping("/all")
-    public Iterable<UserDTO> findAllUsers() {
-        return userService.getAllUsers();
-    }
+    @Operation(
+            summary = "Получение всех пользователей",
+            description = """
+                    Получает всю информацию (кроме паролей)
+                    о всех пользователях в базе данных
+                """
+    )
+    @ApiResponse(responseCode = "200", description = "Все данные получены")
+    Iterable<UserDTO> findAllUsers();
 
     @GetMapping("/{id}")
-    public UserDTO findUserById(@PathVariable("id") int id) {
-        return userService.getById(id);
-    }
+    @Operation(
+            summary = "Получение проекта по id",
+            description = """
+                    Получает всю информацию (кроме пароля) о пользователе
+                    по его id
+                """
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Все данные получены"),
+            @ApiResponse(responseCode = "404", description = "Не найден пользователь по данному id")
+    })
+    UserDTO findUserById(
+            @PathVariable("id") int id
+    );
 }
